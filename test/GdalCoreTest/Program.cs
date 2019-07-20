2193d1e9e94194c75dec961a928ad7ebf3bdde50
MaxRev-Dev <cmthea32@gmail.com>
2019-07-20 08:26:14 +0300

    lost *.cs in test

---
+++ test/GdalCoreTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MaxRev.Gdal.Core;
+using OSGeo.GDAL;
+
+namespace GdalCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                GdalBase.ConfigureAll();
+                 
+                var drivers = new[] { "hdf4", "hdf5", "gtiff", "hf2", "ceos", "http" };
+                foreach (var driver in drivers)
+                {
+                    var driverByName = Gdal.GetDriverByName(driver);
+                    Console.WriteLine(
+                        driverByName != default ? $"{driver} loaded successfully" : $"Failed to load {driver}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
