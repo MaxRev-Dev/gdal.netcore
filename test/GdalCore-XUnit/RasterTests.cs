1498c892e400b2582c2ebdca6eecc29b3f56aeda
MaxRev <cmthea32@gmail.com>
2022-01-05 17:50:06 +0200

    restored utf-8 tests

--- test/GdalCore-XUnit/RasterTests.cs
+++ test/GdalCore-XUnit/RasterTests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System;
 using System.IO;
 using System.Linq;
@@ -5,6 +6,7 @@ using MaxRev.Gdal.Core;
 using OSGeo.GDAL;
 using Xunit;
 using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace GdalCore_XUnit
 {
@@ -32,7 +34,7 @@ namespace GdalCore_XUnit
         {
             Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "YES");
 
-            string currentState = Gdal.GetConfigOption("GDAL_FILENAME_IS_UTF8", "YES"); 
+            string currentState = Gdal.GetConfigOption("GDAL_FILENAME_IS_UTF8", "YES");
 
             _outputHelper.WriteLine($"Test 1 - GDAL_FILENAME_IS_UTF8 is set to {currentState} by default");
 
@@ -69,14 +71,25 @@ namespace GdalCore_XUnit
         [Fact]
         public void CyrillicSymbols_NO_Option()
         {
-            Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "NO"); 
+            Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "NO");
 
             var currentState = Gdal.GetConfigOption("GDAL_FILENAME_IS_UTF8", "YES");
 
             _outputHelper.WriteLine($"Test 4 - GDAL_FILENAME_IS_UTF8 is set to {currentState} before the test");
 
             var outputFilePath = Path.Combine(dataDirectoryPath, "test4.vrt");
-            Assert.False(RunTest(cyrillicInputFilePath, dataDirectoryPath, outputFilePath));
+
+            var result = RunTest(cyrillicInputFilePath, dataDirectoryPath, outputFilePath);
+
+            // this works like a charm on linux even without config flag
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                Assert.True(result);
+
+            // windows can't find a file though
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                Assert.False(result);
+            else
+                throw new XunitException("This test was not created for current os platform");
         }
 
         private bool GdalBuildVrt(string[] inputFilesPaths, string outputFilePath, string[] options, Gdal.GDALProgressFuncDelegate callback)
@@ -118,11 +131,16 @@ namespace GdalCore_XUnit
             string[] inputFilesPaths = new DirectoryInfo(inputDirectoryPath)
                                       .EnumerateFiles().Select(fileInfo => fileInfo.FullName).ToArray();
             if (!GdalBuildVrt(inputFilesPaths, outputFilePath, null, null))
+            {
+                _outputHelper.WriteLine($"GdalBuildVrt error on processing {inputFilesPaths} to {outputFilePath}");
                 isTestSuccessful = false;
-
+            }
             // Check if .vrt file was created, because GdalBuildVrt doesn't throw exceptions in that case
             if (!File.Exists(outputFilePath))
+            {
+                _outputHelper.WriteLine($"File not found {outputFilePath}");
                 isTestSuccessful = false;
+            }
 
             return isTestSuccessful;
         }
