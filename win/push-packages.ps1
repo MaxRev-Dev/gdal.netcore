c70b8e24525457eacf528c6b4e4522e14fd73411
Thea Camille-Maxime (MaxRev) <cmthea32@gmail.com>
2022-12-28 00:42:48 +0200

    Disable missing ODBC driver for MySQL on windows rumtime (#86)

--- win/push-packages.ps1
+++ win/push-packages.ps1
@@ -1,7 +1,15 @@
-dotnet nuget push ../nuget/MaxRev.Gdal.Core.$env:VERSION.nupkg -s nuget.org -k $env:API_KEY_NUGET --skip-duplicate
-dotnet nuget push ../nuget/MaxRev.Gdal.LinuxRuntime.Minimal.$env:VERSION.nupkg -s nuget.org -k $env:API_KEY_NUGET --skip-duplicate
-dotnet nuget push ../nuget/MaxRev.Gdal.WindowsRuntime.Minimal.$env:VERSION.nupkg -s nuget.org -k $env:API_KEY_NUGET --skip-duplicate
-
-dotnet nuget push ../nuget/MaxRev.Gdal.Core.$env:VERSION.nupkg -s github -k $env:API_KEY_GITHUB --skip-duplicate
-dotnet nuget push ../nuget/MaxRev.Gdal.LinuxRuntime.Minimal.$env:VERSION.nupkg -s github -k $env:API_KEY_GITHUB --skip-duplicate
-dotnet nuget push ../nuget/MaxRev.Gdal.WindowsRuntime.Minimal.$env:VERSION.nupkg -s github -k $env:API_KEY_GITHUB --skip-duplicate
+param (
+    [string] $version
+)
+$packages = @(
+    "MaxRev.Gdal.Core.$version.nupkg"
+    "MaxRev.Gdal.LinuxRuntime.Minimal.$version.nupkg"
+    "MaxRev.Gdal.WindowsRuntime.Minimal.$version.nupkg"
+)
+foreach ($package in $packages) {
+    $packagePath = "$PSScriptRoot/../nuget/$package"
+    if (Test-Path -Path $packagePath -PathType Leaf) {
+        dotnet nuget push $packagePath -s nuget.org -k $env:API_KEY_NUGET --skip-duplicate
+        dotnet nuget push $packagePath -s github -k $env:API_KEY_GITHUB --skip-duplicate
+    }
+}
