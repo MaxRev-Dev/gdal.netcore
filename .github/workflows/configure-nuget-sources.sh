fd150fa432153239d34a48b2843a4f2b7dc26dc0
MaxRev <cmthea32@gmail.com>
2024-06-02 00:36:03 +0300

    Updates

--- .github/workflows/configure-nuget-sources.sh
+++
@@ -1,5 +0,0 @@
-(cd ../.. && dotnet new nugetconfig --force)
-dotnet nuget remove source local > /dev/null || :
-dotnet nuget remove source local.ci > /dev/null || :
-dotnet nuget remove source github > /dev/null || :
-dotnet nuget remove source nuget.org > /dev/null || :        
