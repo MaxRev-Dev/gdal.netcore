ada98a5fef005e100e29a3c6224e745f15f467e4
MaxRev <cmthea32@gmail.com>
2024-08-08 23:55:18 +0300

    Updates

--- osx/before-install.sh
+++ osx/before-install.sh
@@ -5,3 +5,6 @@ set -e
 # requirements for CI runner. Removed python3 as it is already installed and causes symlink issues
 brew install make pkg-config autoconf automake \
     autoconf-archive swig libtool dylibbundler gsed pipx
+
+# issue with libtool on macOS https://github.com/Homebrew/homebrew-core/issues/180040
+brew reinstall libtool
