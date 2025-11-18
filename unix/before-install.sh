f108824680ea149ab21047cc1123cc0e7889476c
MaxRev <cmthea32@gmail.com>
2025-11-18 19:17:44 +0200

    Update install script
    [skip windows]
    [skip osx]

--- unix/before-install.sh
+++ unix/before-install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+arch="$(dpkg --print-architecture)"
+case "$arch" in
+     amd64)
+          header_pkg="linux-headers-amd64"
+          ;;
+     arm64)
+          header_pkg="linux-headers-arm64"
+          ;;
+     *)
+          header_pkg="linux-headers-$(uname -r)"
+          echo "Unknown architecture '$arch', falling back to $header_pkg" >&2
+          ;;
+esac
+
 sudo apt-get install g++ make cmake git curl \
-     zip unzip tar pkg-config linux-headers-amd64 \
-     autoconf automake python3 autoconf-archive swig patchelf
+      zip unzip tar pkg-config "$header_pkg" \
+      autoconf automake python3 autoconf-archive swig patchelf
