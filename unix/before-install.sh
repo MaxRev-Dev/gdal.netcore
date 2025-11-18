2a4b8ee86e6cf1582e1f1a31bd6a25e551d7bfa2
MaxRev <cmthea32@gmail.com>
2025-11-18 20:04:18 +0200

    Update install script
    [skip windows]
    [skip osx]

--- unix/before-install.sh
+++ unix/before-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e 
+sudo apt-get update
 sudo apt-get install g++ make cmake git curl \
       zip unzip tar pkg-config linux-headers-generic \
-      autoconf automake python3 autoconf-archive swig patchelf
+      autoconf automake python3 autoconf-archive swig patchelf -y
