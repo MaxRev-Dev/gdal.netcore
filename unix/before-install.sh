c654a042815b125ffabad8319f3cebc81fdd2bf0
MaxRev <cmthea32@gmail.com>
2025-11-18 20:24:08 +0200

    Update workflow
    [skip windows]
    [skip osx]

--- unix/before-install.sh
+++ unix/before-install.sh
@@ -2,5 +2,5 @@
 set -e 
 sudo apt-get update
 sudo apt-get install g++ make cmake git curl \
-      zip unzip tar pkg-config linux-headers-generic \
+      zip unzip tar pkg-config linux-headers-generic libltdl-dev \
       autoconf automake python3 autoconf-archive swig patchelf -y
