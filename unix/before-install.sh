4757c434c1159780b489df384af9c6da5e0b0b19
MaxRev <cmthea32@gmail.com>
2025-11-18 18:52:36 +0200

    Moving dockerized Linux CI to native

---
+++ unix/before-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+sudo apt-get install g++ make cmake git curl \
+     zip unzip tar pkg-config linux-headers-amd64 \
+     autoconf automake python3 autoconf-archive swig patchelf
