a1df00c7ae6a3cec313964ee07184e969e2a95de
Camille-Maxime Thea (MaxRev) <cmthea32@gmail.com>
2025-11-20 18:51:35 +0200

    Hotfix: Restore external HDF4 in linux packages (#206)
    * Restore HDF4
    
    * Install cript updated
    
    * Bump HDF4 version. Configuring HDF4 for GDAL
    
    * Update workflow

--- unix/before-install.sh
+++ unix/before-install.sh
@@ -3,4 +3,4 @@ set -e
 sudo apt-get update
 sudo apt-get install g++ make cmake git curl \
       zip unzip tar pkg-config linux-headers-generic libltdl-dev \
-      autoconf automake python3 autoconf-archive swig patchelf -y
+      autoconf automake python3 autoconf-archive swig patchelf bison -y
